5368478426a5044ab8cc5a4d215b1427d49589a7
eddieljs <2574350328.com>
2025-03-10 11:44:56 +0800

    7.3 增加新增乘车人接口 增加自动校验

--- train-member/src/main/java/com/eddie/train/member/req/PassengerSavaReq.java
+++ train-member/src/main/java/com/eddie/train/member/req/PassengerSavaReq.java
@@ -1,5 +1,7 @@
 package com.eddie.train.member.req;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,12 +14,16 @@ import java.util.Date;
 public class PassengerSavaReq {
     private Long id;
 
+    @NotNull(message = "【会员ID】不能为空")
     private Long memberId;
 
+    @NotNull(message = "【名字】不能为空")
     private String name;
 
+    @NotBlank(message = "【身份证号】不能为空")
     private String idCard;
 
+    @NotBlank(message = "【旅客类型】不能为空")
     private String type;
 
     private Date createTime;
