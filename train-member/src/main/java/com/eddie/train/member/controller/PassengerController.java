00f17b4900feb70ea56ed4879d19c24227026af9
eddieljs <2574350328.com>
2025-03-10 16:08:12 +0800

    7.8 乘车人列表查询后端接口

--- train-member/src/main/java/com/eddie/train/member/controller/PassengerController.java
+++ train-member/src/main/java/com/eddie/train/member/controller/PassengerController.java
@@ -1,14 +1,15 @@
 package com.eddie.train.member.controller;
 
 import com.eddie.train.common.resp.Result;
+import com.eddie.train.member.req.PassengerQueryReq;
 import com.eddie.train.member.req.PassengerSavaReq;
+import com.eddie.train.member.resp.PassengerQueryResp;
 import com.eddie.train.member.service.PassengerService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/passenger")
@@ -21,5 +22,12 @@ public class PassengerController {
     public Result<Object> save(@Valid @RequestBody PassengerSavaReq req) {
         passengerService.save(req);
         return Result.success("新增乘客成功！");
+    }
+
+    @GetMapping("/query-list")
+    public Result<List<PassengerQueryResp>> queryList(@Valid PassengerQueryReq req) {
+//        req.setMemberId(LoginMemberContext.getId());
+        List<PassengerQueryResp> passengerList = passengerService.queryList(req);
+        return Result.success(passengerList);
     }
 }
--- train-member/src/main/java/com/eddie/train/member/service/PassengerService.java
+++ train-member/src/main/java/com/eddie/train/member/service/PassengerService.java
@@ -2,14 +2,20 @@ package com.eddie.train.member.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
 import com.eddie.train.common.context.LoginMemberContext;
 import com.eddie.train.common.util.SnowUtil;
 import com.eddie.train.member.domain.Passenger;
+import com.eddie.train.member.domain.PassengerExample;
 import com.eddie.train.member.mapper.PassengerMapper;
+import com.eddie.train.member.req.PassengerQueryReq;
 import com.eddie.train.member.req.PassengerSavaReq;
+import com.eddie.train.member.resp.PassengerQueryResp;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -27,5 +33,15 @@ public class PassengerService {
         passenger.setCreateTime(now);
         passenger.setUpdateTime(now);
         passengerMapper.insert(passenger);
+    }
+
+    public List<PassengerQueryResp> queryList(PassengerQueryReq req){
+        PassengerExample passengerExample = new PassengerExample();
+        PassengerExample.Criteria criteria = passengerExample.createCriteria();
+        if (ObjectUtil.isNotNull(LoginMemberContext.getId())){
+            criteria.andMemberIdEqualTo(LoginMemberContext.getId());
+        }
+        List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
+        return BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
     }
 }
