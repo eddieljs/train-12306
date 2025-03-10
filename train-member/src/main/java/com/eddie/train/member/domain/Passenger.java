7a45c1d69e53ac174c0fcc4efed93b61164ff21f
eddieljs <2574350328.com>
2025-03-10 10:36:05 +0800

    7.2 增加乘车人表，生成持久层代码

---
+++ train-member/src/main/java/com/eddie/train/member/domain/Passenger.java
@@ -0,0 +1,26 @@
+package com.eddie.train.member.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Passenger {
+    private Long id;
+
+    private Long memberId;
+
+    private String name;
+
+    private String idCard;
+
+    private String type;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+}
