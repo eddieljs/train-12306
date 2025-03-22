d0f0820ebe28338c53a12d8bc707d6c9d58db7b8
eddieljs <2574350328.com>
2025-03-22 12:15:09 +0800

    10.6 DisallowConcurrentExecution注解 禁止任务并发执行

--- train-batch/src/main/java/com/eddie/train/batch/job/TestJob.java
+++ train-batch/src/main/java/com/eddie/train/batch/job/TestJob.java
@@ -1,13 +1,21 @@
 package com.eddie.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+@DisallowConcurrentExecution
 public class TestJob implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-            System.out.println("Testing job");
+        System.out.println("Testing 开始");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Testing 结束");
     }
 }
