6fb0c3f0c300e94f2f4c881598b2739ee427dfda
eddieljs <2574350328.com>
2025-03-29 18:10:47 +0800

    12.21 买票后进行余票扣减

---
+++ train-business/src/main/java/com/eddie/train/business/mapper/cust/DailyTrainTicketMapperCust.java
@@ -0,0 +1,16 @@
+package com.eddie.train.business.mapper.cust;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Date;
+@Mapper
+public interface DailyTrainTicketMapperCust {
+
+    void updateCountBySell(Date date
+            , String trainCode
+            , String seatTypeCode
+            , Integer minStartIndex
+            , Integer maxStartIndex
+            , Integer minEndIndex
+            , Integer maxEndIndex);
+}
