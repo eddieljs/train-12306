99b4b21f952abe0bcba62bb05067f3175f085e5c
eddieljs <2574350328.com>
2025-04-01 18:40:44 +0800

    13.1 集成nacos

--- train-member/src/main/java/com/eddie/train/member/controller/TestController.java
+++ train-member/src/main/java/com/eddie/train/member/controller/TestController.java
@@ -1,10 +1,13 @@
 package com.eddie.train.member.controller;
 
+import lombok.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class TestController {
+
     @GetMapping("/hello")
     public String hello() {return "Hello World!!!";}
 }
